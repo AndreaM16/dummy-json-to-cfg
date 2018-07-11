2dab3b992c473fa1ae20e0b3f9f380b707642edd
andream16 <andrea.medda.uni@gmail.com>
2018-07-11 16:29:57 +0200

    Adding Docs

---
+++ pkg/config/docs.go
@@ -0,0 +1,5 @@
+// Package config handles the process of loading a json file and unmarshalling
+// it into a given struct.
+//
+// This is a really small package and only structs are supported at the moment.
+package config
